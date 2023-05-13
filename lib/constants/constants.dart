c857fa419d98d888c355d6282fee60d6cb94193a
darius35150 <darius35150@yahoo.com>
2023-05-13 01:04:48 -0500

    Added to the list of constants to filter out.

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,4 +5,17 @@ class Constants {
   static String COLUMN = '@Column';
   static String JSON_REF = '@JsonManagedReference';
   static String ONE_TO_MANY = '@OneToMany';
+  static String MANY_TO_ONE = '@ManyToOne';
+  static String SEQ_GENERATOR = '@SequenceGenerator';
+  static String TRANSIENT = '@Transient';
+  static String EMBEDDED_ID = '@EmbeddedId';
+  static String GENERATED_VALUE = '@GeneratedValue';
+  static String JOIN_COLUMN = '@JoinColumn';
+  static String JSON_BACK_REF = '@JsonBackReference';
+  static String JSON_MANAGED_REF = '@JsonManagedReference';
+  static String NOT_NULL = '@NotNull';
+  static String ORACLE_TYPE_ATTRIBUTE = '@OracleTypeAttribute';
+  static String XML_JAVA_TYPE_ADAPTER = 'XmlJavaTypeAdapter';
+  static String XML_TRANSIENT = '@XmlTransient';
+  static String ORG_SPRING_DATA_ANNOTATION_ID = '@org.springframework.data.annotation.Id';
 }
--- lib/convert/convert.dart
+++ lib/convert/convert.dart
@@ -17,8 +17,16 @@ class Convert {
     for(String string in list) {
       if(!string.trim().contains(Constants.COLUMN) && !string.trim().contains(Constants.ID) &&
          !string.trim().contains(Constants.ONE_TO_MANY) && !string.trim().contains(Constants.JSON_REF) &&
-         string.isNotEmpty){
+         !string.trim().contains(Constants.EMBEDDED_ID) && !string.trim().contains(Constants.GENERATED_VALUE) &&
+         !string.trim().contains(Constants.JOIN_COLUMN) && !string.trim().contains(Constants.JSON_BACK_REF) &&
+         !string.trim().contains(Constants.JSON_MANAGED_REF) && !string.trim().contains(Constants.MANY_TO_ONE) &&
+         !string.trim().contains(Constants.NOT_NULL) && !string.trim().contains(Constants.ORACLE_TYPE_ATTRIBUTE) &&
+         !string.trim().contains(Constants.ORG_SPRING_DATA_ANNOTATION_ID) && !string.trim().contains(Constants.SEQ_GENERATOR) &&
+         !string.trim().contains(Constants.TRANSIENT) && !string.trim().contains(Constants.XML_JAVA_TYPE_ADAPTER) &&
+         !string.trim().contains(Constants.XML_TRANSIENT) && string.isNotEmpty){
+
         newList.add(string.trim());
+
       }
     }
     buffer.writeAll(newList, "\n");
